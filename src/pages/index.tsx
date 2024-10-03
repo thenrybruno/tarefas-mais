c4d95435284646c143bf35fc36945e9958593759
Bruno Luiz <you@example.comthenrybruno@hotmail.com.br>
2024-10-03 20:44:22 -0300

    Criação do header e sua estilização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,6 +33,15 @@ export default function Home() {
           Sistema feito para você organizar <br />
           seus estudos e tarefas
         </h1>
+
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+          <section className={styles.box}>
+            <span>+90 comentários</span>
+          </section>
+        </div>
       </main>
     </div>
   );
